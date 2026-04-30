25d4ba66b5f019cb4389a5544b3cffc1679a9564
UsamaAininou <100040018+Usama01TN@users.noreply.github.com>
2026-04-30 18:49:03 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,3 +2,28 @@
 """
 Init project.
 """
+from os.path import dirname
+from sys import path
+
+if dirname(__file__) not in path:
+    path.append(dirname(__file__))
+
+try:
+    from .builders import getMakeExecutable, getPythonExecutable, getOpenExecutable, getHgExecutable, \
+        getPyqtdeployBuildExecutable, getJavaExecutable, getXcodeSelectExecutable, \
+        getXcodebuildExecutable, getCmakeExecutable, getAntExecutable, getUVExecutable, \
+        getGitExecutable, getXcrunExecutable, getNdkBuildExecutable, getCurrentExecutable, \
+        getAdbExecutable, getPyqtdeploySysrootExecutable, getArmLinuxAndroideabiStripExecutable, \
+        getCmakeVersion, CMAKE_BIN_DIR
+except:
+    from builders import getMakeExecutable, getPythonExecutable, getOpenExecutable, getHgExecutable, \
+        getPyqtdeployBuildExecutable, getJavaExecutable, getXcodeSelectExecutable, getXcodebuildExecutable, \
+        getCmakeExecutable, getAntExecutable, getUVExecutable, getGitExecutable, getXcrunExecutable, \
+        getNdkBuildExecutable, getCurrentExecutable, getAdbExecutable, getPyqtdeploySysrootExecutable, \
+        getArmLinuxAndroideabiStripExecutable, getCmakeVersion, CMAKE_BIN_DIR
+
+__all__ = ['getMakeExecutable', 'getPythonExecutable', 'getOpenExecutable', 'getHgExecutable',
+           'getPyqtdeployBuildExecutable', 'getJavaExecutable', 'getXcodeSelectExecutable', 'getXcodebuildExecutable',
+           'getCmakeExecutable', 'getAntExecutable', 'getUVExecutable', 'getGitExecutable', 'getXcrunExecutable',
+           'getNdkBuildExecutable', 'getCurrentExecutable', 'getAdbExecutable', 'getPyqtdeploySysrootExecutable',
+           'getArmLinuxAndroideabiStripExecutable', 'getCmakeVersion', 'CMAKE_BIN_DIR']
