8af898135622dd9f5655491c212b5bfbd66956e4
UsamaAininou <100040018+Usama01TN@users.noreply.github.com>
2026-05-24 14:17:12 +0200

    Update main.py

--- testapp/main.py
+++ testapp/main.py
@@ -1,14 +1,29 @@
-# main.py
+# -*- coding: utf-8 -*-
 import sys
-from PySide6.QtWidgets import QApplication, QLabel
+from PySide import QtGui
 
+class MainWindow(QtGui.QWidget):
+    def __init__(self):
+        super(MainWindow, self).__init__()
 
-def main():
-    app = QApplication(sys.argv)
-    label = QLabel("Hello World!")
-    label.show()
-    sys.exit(app.exec())
+        self.label = QtGui.QLabel("Hello from PySide 1", self)
+        self.button = QtGui.QPushButton("Click me", self)
 
+        self.button.clicked.connect(self.on_click)
 
-if __name__ == "__main__":
-    main()
+        layout = QtGui.QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.button)
+        self.setLayout(layout)
+
+        self.setWindowTitle("PySide 1 Example")
+        self.resize(300, 120)
+
+    def on_click(self):
+        self.label.setText("Button clicked!")
+
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
